5f447a02312cf2d5d879e1462f1e6e49034f902f
Christina <christina.migina@abv.bg>
2026-06-02 23:21:41 +0200

    add json files, generate_diff

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,8 @@
+import json
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+
+    return data1, data2
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,10 @@
 from gendiff.cli import parse_args
+from gendiff.generate_diff import generate_diff
 
 
 def main():
     args = parse_args()
+    generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == "__main__":
